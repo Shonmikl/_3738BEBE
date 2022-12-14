bb26192e3525a2510e7a02c79203260ddab9d68b
shonmikl <mikhailegorov676@gmail.com>
2022-12-14 14:16:54 +0300

    TreeBi

--- src/_14_12_2022/Node.java
+++ src/_14_12_2022/Node.java
@@ -1,6 +1,7 @@
 package _14_12_2022;
 
 public class Node {
+
     public Integer value;
     public Node left;
     public Node right;
@@ -26,5 +27,52 @@ public class Node {
         }
     }
 
+    //todo
+    private static Node search(Node node, int value) {
+        return null;
+    }
 
+    private static Node getMin(Node node) {
+        if(!isNodeExist(node)) {
+            return null;
+        }
+        if((!isNodeExist(node.left))) {
+            return node;
+        }
+        return getMin(node.left);
+    }
+
+    //todo
+    private static Node getMax(Node node) {
+        return null;
+    }
+
+    private static void inOrderTraversal(Node node) {
+        if(!isNodeExist(node)) {
+            return;
+        }
+        inOrderTraversal(node.left);
+        System.out.println("[ " + node + " ]");
+        inOrderTraversal(node.right);
+    }
+
+    //todo
+    private static void postOrderTraversal(Node node) {
+
+    }
+
+    //todo
+    private static void directOrderTraversal(Node node) {
+
+    }
+
+    //todo class
+    private static void moveNode(Node toNode, Node fromNode) {
+
+    }
+
+    //todo class
+    private static int getChildrenCount(Node node) {
+        return 0;
+    }
 }
