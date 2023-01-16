4d14c7c20931db98638b32f127de44568a2af253
shonmikl <mikhailegorov676@gmail.com>
2023-01-16 14:42:34 +0300

    Searcher.java

--- src/_16_01_2023/Searcher.java
+++ src/_16_01_2023/Searcher.java
@@ -6,13 +6,26 @@ import java.util.List;
 
 public class Searcher {
 
-    private static void getFiles(File file, List<File> fileList) {
-
+    private static void getFiles(File root, List<File> fileList) {
+        if (root.isDirectory()) {
+            //System.out.println("searching....." + root.getAbsolutePath());
+            File[] direcoriFile = root.listFiles();
+            if (direcoriFile != null) {
+                for (File file : direcoriFile) {
+                    if (file.isDirectory()) {
+                        getFiles(file, fileList);
+                    } else {
+                        if (file.getName().toLowerCase().startsWith("release"));
+                        fileList.add(file);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        ArrayList<File> list = new ArrayList<File>();
-        getFiles(new File("E:\\"), list);
+        ArrayList<File> list = new ArrayList<>();
+        getFiles(new File("D:\\"), list);
 
         for (File file : list) {
             System.out.println(file.getAbsolutePath());
