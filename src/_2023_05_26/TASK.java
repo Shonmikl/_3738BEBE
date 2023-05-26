338fce41da48ebe4f6caaf00d2bbc902f4bdd3b7
shonmikl <mikhailegorov676@gmail.com>
2023-05-26 11:30:54 +0200

    task

---
+++ src/_2023_05_26/TASK.java
@@ -0,0 +1,25 @@
+package _2023_05_26;
+
+public class TASK {
+
+    /**
+     * 123 --- 321
+     * 210 --- 12
+     * -456 --- -654
+     */
+
+    private static int reverseNumber(int x) {
+        return 0;
+    }
+
+    /**
+     * [ 1 2 3 4 5 5]
+     * out 5
+     */
+
+    private static int duplicated(int[] array) {
+        return 0;
+    }
+
+
+}
